f239663dc7111625fbb45869445bb067ab150e62
cr0sh <ska827@naver.com>
2017-10-01 21:18:04 +0900

    Add raknet packet protocols

---
+++ raknet/proto.go
@@ -0,0 +1,89 @@
+package raknet
+
+import (
+	"bytes"
+	"errors"
+	"github.com/cr0sh/encore/util/binary"
+	"net"
+	"strconv"
+)
+
+// Packet ID: 0x00
+type ConnectedPing struct {
+	SendPingTime uint64
+}
+
+// Packet ID: 0x01
+type UnconnectedPing struct {
+	PingID     uint64
+	OfflineMsg offlineMessageDataID
+}
+
+// Packet ID: 0x03
+type ConnectedPong struct {
+	SendPingTime uint64
+	SendPongTime uint64
+}
+
+// Packet ID: 0x1c
+type UnconnectedPong struct {
+	PingID     uint64
+	ServerID   uint64
+	OfflineMsg offlineMessageDataID
+	ServerName binary.FixedMCString
+}
+
+// Packet ID: 0x05
+type OpenConnectionRequest1 struct {
+	OfflineMsg   offlineMessageDataID
+	ProtoVersion byte
+}
+
+// Packet ID: 0x06
+type OpenConnectionReply1 struct {
+	OfflineMsg offlineMessageDataID
+	ServerGUID uint64
+	Security   bool
+	MTU        uint16
+}
+
+// Packet ID: 0x07
+type OpenConnectionRequest2 struct {
+	OfflineMsg offlineMessageDataID
+}
+
+// Packet ID: 0x08
+type OpenConnectionRePly2 struct {
+	OfflineMsg offlineMessageDataID
+	ServerGUID uint64
+	ClientAddr IPAddr
+	MTU        uint16
+	Security   bool
+}
+
+// Packet ID: 0x09
+type ConnectRequest struct {
+	ClientGUID   uint64
+	SendPingTime uint64
+	Security     bool
+}
+
+// Packet ID: 0x10
+type ServerHandshake struct {
+	SystemAddr   IPAddr
+	SystemIdx    uint16
+	SystemAddrs  systemAddresses
+	SendPingTime uint64
+	SendPongTime uint64
+}
+
+// Packet ID: 0x13
+type ClientHandshake struct {
+	ClientAddr   IPAddr
+	SystemAddrs  systemAddresses
+	SendPingTime uint64
+	SendPongTime uint64
+}
+
+// Packet ID: 0x15
+type ClientDisconnect struct{}
---
+++ raknet/raknet.go
@@ -0,0 +1,78 @@
+// package raknet implements RakNet protocol for servers/clients.
+// Its main usage and implementation goal is for servers,
+// but it's designed to be used for both.
+//
+// Many protocol informations are from:
+//  https://github.com/Nukkit/Nukkit
+//  https://github.com/NiclasOlofsson/MiNET
+package raknet
+
+const OFFLINE_MESSAGE_DATA_ID = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78"
+
+type offlineMessageDataID struct{}
+
+func (offlineMessageDataID) MarshalStream(buf *bytes.Buffer) (err error) {
+	_, err = buf.WriteString(OFFLINE_MESSAGE_DATA_ID)
+	return
+}
+
+func (*offlineMessageDataID) UnmarshalStream(buf *bytes.Buffer) (err error) {
+	buf.Next(len(OFFLINE_MESSAGE_DATA_ID))
+	return nil
+}
+
+// IPAddr represents a single UDP endpoint address in raknet.
+type IPAddr net.UDPAddr
+
+// MarshalStream implements Stream Marshaler interface.
+func (a IPAddr) MarshalStream(buf *bytes.Buffer) error {
+	v4ip := a.IP.To4() // Currently only supports IPv4
+	b := make([]byte, 7)
+	b[0] = 4
+	copy(b[1:5], v4ip[:4])
+	binary.BigEndian.PutUint16(b[5:7], uint16(a.Port))
+	_, err := buf.Write(b)
+	return err
+}
+
+// UnmarshalStream implements Strream Unmarshaler interface.
+func (a *IPAddr) UnmarshalStream(buf *bytes.Buffer) (err error) {
+	if a == nil {
+		a = new(IPAddr)
+	}
+	b := make([]byte, 7)
+	if _, err = buf.Read(b); err != nil {
+		return
+	}
+	if b[0] != 4 {
+		return errors.New("IPAddr only supports IPv4, v" + strconv.Itoa(int(b[0])) + " given")
+	}
+
+	a.IP = net.IPv4(^b[1], ^b[2], ^b[3], ^b[4])
+	a.Port = int(binary.BigEndian.Uint16(b[5:7]))
+
+	return nil
+}
+
+const systemAddressesReady = "\x04\x80\xff\xff\xfe\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00" +
+	"\x04\xff\xff\xff\xff\x00\x00"
+
+type systemAddresses struct{}
+
+func (systemAddresses) MarshalStream(buf *bytes.Buffer) (err error) {
+	_, err = buf.WriteString(systemAddressesReady)
+	return
+}
+
+func (*systemAddresses) UnmarshalStream(buf *bytes.Buffer) error {
+	buf.Next(len(systemAddressesReady))
+	return nil
+}
